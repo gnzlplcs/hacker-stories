e16f99b50236063b2b3027646571b20d17d045ec
gnzlplcs <palaciosgm@outlook.com>
2023-01-24 06:02:35 -0500

    :construction: handler function in JSX section completed

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-const Search = () => (
-  <div>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
-  </div>
-);
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event); // synthetic event
+    console.log(event.target.value); // value of target (here: input HTML element)
+  };
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange}/>
+    </div>
+  );
+};
 
 export default Search;
