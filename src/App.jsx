284e618651c8f0e50c1ea0b42b6691d23308daf1
gnzlplcs <palaciosgm@outlook.com>
2023-01-24 01:56:26 -0500

    :construction: react jsx section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const title = 'React'
+const title = "React";
+
+const welcome = {
+  greeting: "Hey",
+  title: "React",
+};
 
 const App = () => {
   return (
     <div>
-      <h1>Hello {title}</h1>
-    </div>
-  )
-}
+      <h1>{welcome.greeting} {welcome.title}</h1>
 
-export default App
+      <label htmlfor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+};
+
+export default App;
