898d108922cb7c9c18dd981bdd36edadd0c24926
gnzlplcs <palaciosgm@outlook.com>
2023-01-24 02:13:48 -0500

    :construction: lists in React section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,48 @@
 import React from "react";
 
-const title = "React";
-
-const welcome = {
-  greeting: "Hey",
-  title: "React",
-};
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 const App = () => {
   return (
     <div>
-      <h1>{welcome.greeting} {welcome.title}</h1>
+      <h1>My Hacker Stories</h1>
 
-      <label htmlfor="search">Search: </label>
+      <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+
+      <ul>
+        {list.map((item) => {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>,{" "}
+              </span>
+              <span>{item.author}, </span>
+              <span>comments: {item.num_comments}, </span>
+              <span>points: {item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
